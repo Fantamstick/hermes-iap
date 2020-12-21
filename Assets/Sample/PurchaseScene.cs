03a4e7b5f5943797d731298d6a7aee2af8ac315e
Jonathan Bro <jonathan.b@fantamstick.jp>
2020-12-21 09:57:55 +0900

    Add sample scene and script

---
+++ Assets/Sample/PurchaseScene.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseScene : MonoBehaviour
+{
+    [SerializeField] private Button initButton;
+    [SerializeField] private Button purchaseButton;
+    [SerializeField] private Button restoreButton;
+    [SerializeField] private Text resultText;
+    private List<string> resultList = new List<string>();
+    
+    public void OnClickInit()
+    {
+        resultList.Add("click init");
+        UpdateText();
+    }
+
+    public void OnClickPurchase()
+    {
+        resultList.Add("click purchase");
+        UpdateText();
+    }
+
+    public void OnClickRestore()
+    {
+        resultList.Add("click restore");
+        UpdateText();
+    }
+
+    public void OnClearText()
+    {
+        resultList.Clear();
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        foreach (var result in resultList)
+        {
+            resultText.text += result + "¥n";
+        }
+    }
+}
