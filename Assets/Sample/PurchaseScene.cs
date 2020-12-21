04eabf7a592d5f55b9094770fc33b178c7d6db86
Jonathan Bro <jonathan.b@fantamstick.jp>
2020-12-21 17:56:58 +0900

    Update sample scene with updated iap

--- Assets/Sample/PurchaseScene.cs
+++ Assets/Sample/PurchaseScene.cs
@@ -1,44 +1,121 @@
 ﻿using System.Collections.Generic;
+using FantamIAP;
 using UnityEngine;
+using UnityEngine.Purchasing;
+using UnityEngine.Purchasing.Security;
 using UnityEngine.UI;
 
+/// <summary>
+/// Test scene for IAP.
+/// </summary>
 public class PurchaseScene : MonoBehaviour
 {
-    [SerializeField] private Button initButton;
-    [SerializeField] private Button purchaseButton;
-    [SerializeField] private Button restoreButton;
+    [SerializeField] private string productId;
+    [Space(30)]
     [SerializeField] private Text resultText;
+    [SerializeField] private Text productIdLabel;
     private List<string> resultList = new List<string>();
-    
+
+    private void Start()
+    {
+        OnClearTextClicked();
+
+        productIdLabel.text = productId;
+        
+        IAPManager.Instance.OnPurchased += OnPurchased;
+    }
+
+    //========================================================
+    // INIT
+    //========================================================
     public void OnClickInit()
     {
-        resultList.Add("click init");
-        UpdateText();
+        UpdateText("click init, waiting for response...");
+        
+        var products = new Dictionary<string, ProductType>
+        {
+            {productId, ProductType.Subscription}
+        };
+
+        var iapBuilder = new IAPBuilder(products).WithAppleTangleData(AppleTangle.Data());
+        IAPManager.Instance.Init(iapBuilder, OnInit);
     }
 
+    void OnInit(InitStatus status)
+    {
+        UpdateText($"IAP init status {status}");
+    }
+    
+    //========================================================
+    // PURCHASE
+    //========================================================
     public void OnClickPurchase()
     {
-        resultList.Add("click purchase");
-        UpdateText();
+        UpdateText("click purchase, waiting for response...");
+        
+        var request = IAPManager.Instance.PurchaseProduct(productId);
+        if (request != PurchaseRequest.Ok)
+        {
+            UpdateText($"problem with purchase: {request}");
+        }
     }
 
+    void OnPurchased(PurchaseResponse resp, Product product)
+    {
+        UpdateText($"purchase result {resp} for Product: {product.transactionID}. {product.definition.id}");
+    }
+    
+    //========================================================
+    // RESTORE
+    //========================================================
     public void OnClickRestore()
     {
-        resultList.Add("click restore");
-        UpdateText();
+        UpdateText("clicked restore, waiting for response...");
+        IAPManager.Instance.RestorePurchases(20_000, onDone: (resp) =>
+        {
+            UpdateText($"Restore attempt, {resp} from product");
+
+            OnClickGetExpiration();
+        });
     }
 
-    public void OnClearText()
+    //========================================================
+    // GET EXPIRATION
+    //========================================================
+    public void OnClickGetExpiration()
+    {
+        var expDate = IAPManager.Instance.GetSubscriptionExpiration(productId);
+        if (expDate.HasValue)
+        {
+            UpdateText($"{productId} expiration date = {expDate.Value}");
+        }
+        else
+        {
+            UpdateText($"{productId} has no expiration date");
+        }
+    }
+
+    //========================================================
+    // UTILITY
+    //========================================================
+    void UpdateText(string str = default)
+    {
+        if (!string.IsNullOrEmpty(str))
+        {
+            resultList.Add(str);
+            Debug.Log(str);
+        }
+
+        resultText.text = "";
+        foreach (var result in resultList)
+        {
+            resultText.text += result + "\n";
+        }
+    }
+
+    public void OnClearTextClicked()
     {
         resultList.Clear();
         UpdateText();
-    }
-
-    void UpdateText()
-    {
-        foreach (var result in resultList)
-        {
-            resultText.text += result + "¥n";
-        }
     }
 }
