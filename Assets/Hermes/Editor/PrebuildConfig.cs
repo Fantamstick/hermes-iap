117162f0acdc9a81dd072b6cb8852e7755d9a76c
Jonathan Bro <jonathanbro@gmail.com>
2021-06-02 15:07:39 +0900

    Add prebuild configuration to switch between GooglePlay and Amazon Store

---
+++ Assets/Hermes/Editor/PrebuildConfig.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+#if UNITY_ANDROID
+using UnityEditor.Purchasing;
+using UnityEngine;
+using UnityEngine.Purchasing;
+#endif
+
+/// <summary>
+/// Prebuild Hermes configuration.
+/// </summary>
+public class PrebuildConfig : IPreprocessBuildWithReport {
+    public int callbackOrder => 0;
+    
+    public void OnPreprocessBuild(BuildReport report) {
+#if UNITY_ANDROID
+#if AMAZON
+        UnityPurchasingEditor.TargetAndroidStore(AppStore.AmazonAppStore);
+        Debug.Log("Success: IAP Targeting Amazon Store");
+#else
+        UnityPurchasingEditor.TargetAndroidStore(AppStore.GooglePlay);
+        Debug.Log("Success: IAP Targeting GooglePlay Store");
+#endif
+#endif
+    }
+}
